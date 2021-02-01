53dc7ee1cbd58f9f9ca04d7df0e83b1133a296d4
arshiya923 <gh.arshiya92@gmail.com>
2021-02-01 17:00:22 +0330

    Update commands.php

--- scripts/commands.php
+++ scripts/commands.php
@@ -13,10 +13,10 @@
         file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$payam);
     }
     else if (strpos($message, "/hava") === 0) {
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ");
         $city = substr($message, 5);
         $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$city."&appid=2f24c59900e19fa5b545708c99edad89"), TRUE)["weather"]["main"];
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$city.": ". $weather);
+        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$weather);
+        file_get_contents(file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$city.": ". $weather));
     }
-    echo 'a';
+    echo 'b';
 ?>

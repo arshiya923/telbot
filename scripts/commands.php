0696a3ad684d39ed34bbbe5dc583e91ffe19faf2
arshiya923 <gh.arshiya92@gmail.com>
2021-02-01 18:54:50 +0330

    Update commands.php

--- scripts/commands.php
+++ scripts/commands.php
@@ -21,7 +21,7 @@
         }
         $weather_json_encoded = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$city."&appid=2f24c59900e19fa5b545708c99edad89");
         $weather_json_decoded = json_decode($weather_json_encoded, TRUE);
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$weather_json_decoded);
+        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$weather_json_encoded);
         $weather_eng = $weather_json_decoded['weather'][0]['main'];
 
         $weather_farsi = '';

b1478d961f13c3bed53d810a0f5e35ce1e745835
arshiya923 <gh.arshiya92@gmail.com>
2021-02-01 16:52:05 +0330

    Update commands.php

--- scripts/commands.php
+++ scripts/commands.php
@@ -6,11 +6,11 @@
     $message = $update["message"]["text"];
 
     if (strpos($message, "/chatid") == 0) {
-        file_get_contents("location:".$tel_api."/sendmessage?chat_id=".$chatId."&text=".$chatId);
+        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$chatId);
     }
     else if (strpos($message, "/bego") == 0) {
         $payam = substr($message, 5);
-        file_get_contents("location:".$tel_api."/sendmessage?chat_id=".$chatId."&text=".$payam);
+        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$payam);
     }
     else if (strpos($message, "/hava") == 0) {
         file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ");

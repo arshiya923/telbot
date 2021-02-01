2c3ba3386f0cddba3f1d5ebc27a14f6a7ac8f4f8
arshiya923 <gh.arshiya92@gmail.com>
2021-02-01 17:52:19 +0330

    Update commands.php

--- scripts/commands.php
+++ scripts/commands.php
@@ -34,6 +34,6 @@
 
         $text = "هوای ".$city." ".$weather_farsi." است.";
         
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$city." : ". $weather_farsi);
+        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$city." : ". $text);
     }
 ?>

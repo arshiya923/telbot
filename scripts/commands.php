f8a4cdf7c686e44802e98d49956936ebc52eb42c
arshiya923 <gh.arshiya92@gmail.com>
2021-02-01 16:35:44 +0330

    Create commands.php

---
+++ scripts/commands.php
@@ -0,0 +1,15 @@
+<?php
+    $tel_api = 'https://api.telegram.org/bot1447496536:AAE6dOUlrgVKdEesnLJsXN6s-zIIgUXC-8w';
+    $update = json_decode(file_get_contents('php://input'),TRUE);
+    
+    $chatId = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+
+    if (strpos($message, "/chatid") === 0) {
+        file_get_contents("location:".$tel_api."/sendmessage?chat_id=".$chatId."&text=".$chatId);
+    }
+    else if (strpos($message, "/bego") === 0) {
+        $payam = substr($message, 5);
+        file_get_contents("location:".$tel_api."/sendmessage?chat_id=".$chatId."&text=".$payam);
+    }
+?>

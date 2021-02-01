a7d9be59c01389a45b2abbedc270ac0f4529f1a3
arshiya923 <gh.arshiya92@gmail.com>
2021-02-01 17:26:06 +0330

    Update commands.php

--- scripts/commands.php
+++ scripts/commands.php
@@ -4,8 +4,6 @@
 
     $chatId = $update["message"]["chat"]["id"];
     $message = $update["message"]["text"];
-    
-    file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=h");
 
     if (strpos($message, "/chatid") === 0) {
         file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$chatId);
@@ -19,10 +17,6 @@
         $weather_json_encoded = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$city."&appid=2f24c59900e19fa5b545708c99edad89");
         $weather_json_decoded = json_decode($weather_json_encoded, TRUE);
         $weather = $weather_json_decoded['weather'][0]['main'];
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$weather_json_encoded);
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$weather_json_decoded);
-        file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=".$weather);
         file_get_contents($tel_api."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$city." : ". $weather);
     }
-    echo 'b';
 ?>
